4126bebbe7d62ee0c306402f7c48eb05c6c82d4c
yhppjk <342089572@qq.com>
2023-03-14 17:36:23 +0100

    try to create mux

--- not_ready/mux2to1_tb.vhd
+++ not_ready/mux2to1_tb.vhd
@@ -17,45 +17,62 @@ USE ieee.numeric_std.ALL;
 --! dec_tb entity description
 --! Detailed description of this
 --! dec_tb design element.
-ENTITY dec_tb IS
-END ENTITY dec_tb;
+ENTITY mux2to1_tb IS
+END ENTITY mux2to1_tb;
 
---! @brief Architecture definition of dec_tb
+--! @brief Architecture definition of mux2to1_tb
 --! @details Testbench implementation
-ARCHITECTURE behavior OF dec_tb IS 
+ARCHITECTURE behavior OF mux2to1_tb IS 
  
-   CONSTANT TBWDITH : POSITIVE := 5;	--! Component declaration for the Design Under Test (DUT
-   COMPONENT dec
+   CONSTANT TBWDITH : POSITIVE := 4;	--! Component declaration for the Design Under Test (DUT
+   COMPONENT mux2to1
    GENERIC	(
 	width : POSITIVE := TBWDITH --! constant to describe the width of decoder
 );
    PORT (
-	din :  IN  std_logic_vector(width-1 downto 0);		--! data input port 
-	dout : OUT std_logic_vector(2**width-1 downto 0)	--! data output port
+	din1 :  IN std_logic_vector(width-1 downto 0);		--! data input port
+	din2 : 	IN std_logic_vector(width-1 downto 0);
+	sel : IN std_logic;
+	dout : OUT std_logic_vector(width-1 downto 0)	--! data output port
 );
-    END COMPONENT dec;
+    END COMPONENT mux2to1;
     
    --Inputs
-   signal din : std_logic_vector(TBWDITH-1 downto 0);	--! data input signal
+   signal din1 : std_logic_vector(TBWDITH-1 downto 0);	--! data input signal
+   signal din2 : std_logic_vector(TBWDITH-1 downto 0);	--! data input signal
+   signal sel  : std_logic :='0';
  	--Outputs
-   signal dout : std_logic_vector(2**TBWDITH-1 downto 0);	--! data output signal
+   signal dout : std_logic_vector(TBWDITH-1 downto 0) ;	--! data output signal
  
 BEGIN
  
 	--! Instantiate the Design Under Test (DUT) and map its ports
-	dut: dec
+	dut: mux2to1
 	PORT MAP ( --! Mapping: component port (left) => this arch signal/port (right)
-		din  => din,
+		din1  => din1,
+		din2  => din2,
+		sel => sel,
 		dout => dout
 	);
 
+	
 --! @brief process test all values to decoder 
 --! @details process test all values to decoder, with an assert statement to stop the simulation  
 	stim_proc: process
 	begin
 
-	loop2 : FOR i IN 0 to 2**TBWDITH-1 LOOP
-	din <= std_logic_vector(to_unsigned(i,TBWDITH));
+	
+	din1 <= std_logic_vector(to_unsigned(5,TBWDITH));
+	din2 <= std_logic_vector(to_unsigned(3,TBWDITH));
+	
+	loop2 : FOR i IN 0 to 30 LOOP
+	
+	sel <= not(sel);
+	if(sel = '0') then
+		dout <= din1;
+	else 
+		dout <= din2;
+	end if;
 	WAIT FOR 20 ns;
 	END LOOP loop2;
 --! an assert statement to stop the simulation
