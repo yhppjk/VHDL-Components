a11600f3e0e1fbe42e691922345f78b270dad334
yhppjk <342089572@qq.com>
2023-05-11 17:16:15 +0200

    coding interface

--- RISC-V with VHDL/test/interface/interface.vhd
+++ RISC-V with VHDL/test/interface/interface.vhd
@@ -45,10 +45,126 @@ entity memory_interface  is
 end entity;
 
 architecture behavioral of memory_interface  is
+	signal WORDADDR : std_logic_vector(29 downto 0);	--high 30 bits of addr_i
+	signal ALIGNMENT : std_logic_vector(1 downto 0);	--low 2 bits of addr_i
+	signal SIZESTRB : std_logic_vector(7 downto 0);		--8 bits encoding of byte strobes in a word
+	signal BYTESTRB : std_logic_vector(7 downto 0);		--8 bits left-shifted value of SIZESTRB
+	signal SECOND_OP: std_logic;						--flag for second operation
+	
+	signal WDATA64 : std_logic_vector(63 downto 0); 	--
+	signal RDATA64 : std_logic_vector(63 downto 0);		--
+	signal PRDATA0 : std_logic_vector(31 downto 0);		--
+	signal PRDATA1 : std_logic_vector(31 downto 0);		--
+	
+	signal RDATA64ALIGNED : std_logic_vector(63 downto 0);
+	
+	constant zeros8 : std_logic_vector(7 downto 0) := (others => '0');
+	
+	TYPE state_type is(idle, setup_transfer, release_enable, return_data);
+	signal current_state, next_state : state_type := idle;
 
 BEGIN
-    
+--divide the 2 data?
+	--clock and reset
+	
+	
+	process(clk, rst)
+	begin
+		if rst = '1' then
+			current_state <= idle;
+		elsif rising_edge(clk) then
+			current_state <= next_state;
+		end if;
+	end process;
+	
+	process(current_state,next_state, rd_i, wr_i, addr_i, size_i, unsigned_i, wdata_i, PRDATA, PREADY, PENABLE)
+	begin
+	--default assignment
+	
 
-
-
+	
+	case current_state is
+		when idle =>
+				WORDADDR <= addr_i(31 downto 2);
+				ALIGNMENT <= addr_i(1 downto 0);
+				case size_i is 
+					when "00" =>
+						SIZESTRB <= "00000001";
+					when "01" =>
+						SIZESTRB <= "00000011";
+					when "11" =>
+						SIZESTRB <= "00001111";
+					when "10" =>
+						SIZESTRB <= "00001111";
+				end case;	
+				
+				BYTESTRB <= shift_left(SIZESTRB, to_integer(unsigned(ALIGNMENT)));	
+				if BYTESTRB(7 downto 4) != "0000" then
+					SECOND_OP <= '1';
+				else 
+					SECOND_OP <= '0';
+				end if;
+				
+				case ALIGNMENT is
+					when "00" =>
+						WDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & wdata_i ;
+						RDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & PRDATA0 ; 
+					when "01" =>
+						WDATA64 <= zeros8 & zeros8 & zeros8 & wdata_i & zeros8 ;
+						if size_i = "00" then
+							RDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & PRDATA0 ; 
+						elsif size_i = "01" then 
+							RDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & PRDATA0 ;
+						else 
+							RDATA64 <= PRDATA1 & PRDATA0 ;
+						end if; 
+					when "10" =>
+						WDATA64 <= zeros8 & zeros8 & wdata_i & zeros8 & zeros8 ;
+						if size_i = "00" then
+							RDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & PRDATA0 ; 
+						elsif size_i = "01" then 
+							RDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & PRDATA0 ;
+						else 
+							RDATA64 <= PRDATA1 & PRDATA0 ;
+						end if; 
+					when "11" =>
+						WDATA64 <= zeros8 & wdata_i & zeros8 & zeros8 & zeros8 ;
+						if size_i = "00" then
+							RDATA64 <= zeros8 & zeros8 & zeros8 & zeros8 & PRDATA0 ; 
+						elsif size_i = "01" then 
+							RDATA64 <= PRDATA1 & PRDATA0 ;
+							RDATA64ALIGNED <=  shift_right(RDATA64, 24);
+						else 
+							RDATA64 <= PRDATA1 & PRDATA0 ;
+							RDATA64ALIGNED <=  shift_right(RDATA64, 24);
+						end if; 
+				end case;	
+				
+				PWRITE <= '0';
+				PENABLE <= '0';
+				PADDR <= WORDADDR & ALIGNMENT;
+				PSTRB <= "0000";
+				
+			if rd_i = '1'or wr_i = '1' then
+				next_state <= setup_transfer;
+			--elsif rd_i = '0' and wr_i = '1' then
+			--	next_state <= setup_transfer;
+			end if;
+			
+		when setup_transfer =>
+			PWDATA <= wdata_i(31 downto 0);
+			PWRITE <= wr_i;
+			PENABLE <= '1';
+			busy_o <= '1';
+			
+			if PREADY = '1' then 
+				next_state <= wait_ready;
+			end if;				
+		when release_enable =>
+		
+		when return_data =>
+		
+	end case;
+	
+	end process;
 end architecture;
